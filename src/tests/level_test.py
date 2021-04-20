1560b58ce1b64f552bd93e987d5673767d9ea9cd
Emil Kivelä <emil.kivela@helsinki.fi>
2021-04-20 23:22:11 +0300

    test code formatting

--- src/tests/level_test.py
+++ src/tests/level_test.py
@@ -23,18 +23,18 @@ CELL_SIZE = 32
 class TestLevel(unittest.TestCase):
     def setUp(self):
         self.level_room = Level(LEVEL_ROOM, CELL_SIZE)
-    
+
     def assert_coordinates_equal(self, sprite, x, y):
-        self.assertEqual(sprite.rect.x ,x)
+        self.assertEqual(sprite.rect.x, x)
         self.assertEqual(sprite.rect.y, y)
 
     def test_can_move_in_floor(self):
         wizard = self.level_room.wizard
-    
+
         self.assert_coordinates_equal(wizard, CELL_SIZE, CELL_SIZE)
 
-        self.level_room.move_player(dx = 2)
+        self.level_room.move_player(dx=2)
         self.assert_coordinates_equal(wizard, 34, 32)
-        
-        self.level_room.move_player(dy = 2)
+
+        self.level_room.move_player(dy=2)
         self.assert_coordinates_equal(wizard, 34, 34)
