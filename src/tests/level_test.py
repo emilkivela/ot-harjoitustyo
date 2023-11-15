a2b8559015f529e3ab9b4bdfb7b3fda60243a493
Emil Kivelä <emil.kivela@gmail.com>
2023-11-15 10:41:15 +0200

    Ensimmäinen testi

---
+++ src/tests/level_test.py
@@ -0,0 +1,40 @@
+import unittest
+from level import Level
+
+
+LEVEL_ROOM = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+              [1, 2, 0, 0, 0, 0, 0, 4, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+CELL_SIZE = 32
+
+class TestLevel(unittest.TestCase):
+    def setUp(self):
+        self.level_room = Level(LEVEL_ROOM, CELL_SIZE)
+
+    def assert_coordinates_equal(self, sprite, x, y):
+        self.assertEqual(sprite.rect.x, x)
+        self.assertEqual(sprite.rect.y, y)
+
+    def test_can_move_in_floor(self):
+        wizard = self.level_room.wizard
+
+        self.assert_coordinates_equal(wizard, CELL_SIZE, CELL_SIZE)
+
+        self.level_room.move_player(dx=2)
+        self.assert_coordinates_equal(wizard, 34, 32)
+
+        self.level_room.move_player(dy=2)
+        self.assert_coordinates_equal(wizard, 34, 34)
